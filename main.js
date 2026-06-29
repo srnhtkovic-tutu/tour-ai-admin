2edb1805f6378ed8e01c946b9671b0d2ddef8fc5
srnhtkovic-tutu <s.rnh.t.kovic@gmail.com>
2026-06-29 09:01:41 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -28,6 +28,52 @@ async function loadSpots(){
 
     console.log(data);
 
+    showSpotList(data);
 }
 
 loadSpots();
+
+function showSpotList(spots){
+
+    const tbody =
+        document.getElementById("spotBody");
+
+    tbody.innerHTML="";
+
+    for(const spot of spots){
+
+        const row=document.createElement("tr");
+
+        row.innerHTML=`
+
+<td>${spot.id}</td>
+
+<td>${spot.name}</td>
+
+<td>${Number(spot.lat).toFixed(6)}</td>
+
+<td>${Number(spot.lng).toFixed(6)}</td>
+
+<td>
+
+<button onclick="editSpot(${spot.id})">
+
+編集
+
+</button>
+
+</td>
+
+`;
+
+        tbody.appendChild(row);
+
+    }
+
+}
+
+function editSpot(id){
+
+    alert(id + " を編集します");
+
+}
